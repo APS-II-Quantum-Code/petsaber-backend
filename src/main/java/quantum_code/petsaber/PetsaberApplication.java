cc33fba960a0120ee1d9cb211e5cac09d7a56e26
Felipe Sandes <felipesand824@gmail.com>
2025-09-24 20:06:30 -0300

    first commit

---
+++ src/main/java/quantum_code/petsaber/PetsaberApplication.java
@@ -0,0 +1,13 @@
+package quantum_code.petsaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsaberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quantum_code/petsaber/PetsaberApplicationTests.java
@@ -0,0 +1,13 @@
+package quantum_code.petsaber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
