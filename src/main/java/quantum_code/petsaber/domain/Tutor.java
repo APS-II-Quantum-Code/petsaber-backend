dd382308a87c0974a4378f6b7636fa6af0b1c318
Felipe Sandes <felipesand824@gmail.com>
2025-09-24 20:30:45 -0300

    feature: entidades tutor & consultor

---
+++ src/main/java/quantum_code/petsaber/domain/Tutor.java
@@ -0,0 +1,23 @@
+package quantum_code.petsaber.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_TUTOR")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idTutor;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+}
